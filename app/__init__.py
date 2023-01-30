c2f33957ba8f7ad8ebec9f565f43ab8fb246701f
@robertsilvatech <robertsilva.info@gmail.com>
2023-01-30 11:33:22 -0300

    add path example for use in class ingress

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask import render_template
+from flask import request
 
 def create_app():
     app = Flask(__name__)
@@ -14,7 +15,21 @@ def create_app():
     def hello(name=None):
         color = os.getenv('COLOR')
         return render_template('index.html',name=name, color=color)
-    
+
+    @app.route('/api')
+    @app.route('/api/v3')
+    @app.route('/api/v4')
+    @app.route('/api/v3/<action>')
+    @app.route('/api/v4/<action>')
+    @app.route('/api/v4/<action>/teste')
+    def path(action=None):
+        base_url = request.base_url
+        if action:
+            return render_template('path.html',base_url=base_url, action=action)
+        else:
+            return render_template('path.html',base_url=base_url)
+        
+
     return app
 
 app = Flask(__name__)
